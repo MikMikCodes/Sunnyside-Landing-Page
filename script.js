59c4f28d27afa174c237b150800e280660d9b2c9
MikMik.Codes <87323916+MikMikCodes@users.noreply.github.com>
2022-02-10 00:04:46 -0600

    uploaded initial files

---
+++ script.js
@@ -0,0 +1,24 @@
+function peekaMenu() {
+ var menu = document.getElementById('menu-container'); 
+      
+
+       if (menu.className === 'hide-menu') {
+           menu.className = 'show-menu';
+       } else {
+           menu.className = 'hide-menu';
+       }
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
